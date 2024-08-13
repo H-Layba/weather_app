012ec4ef9c4423f941452e71bb38a8e23d8c3cd9
PMYLS <laybahudafjwu@gmail.com>
2024-08-13 21:40:54 +0500

    -

---
+++ lib/constants/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/foundation.dart' show immutable;
+
+@immutable
+class Constants {
+  static const String apiKey = '3fbe1f5beb3c5c231096ffcdf0748111';
+  static const String apiKey2 = '44e194828b25cdaca4b96ed41da3bfce';
+}
+
+
+
